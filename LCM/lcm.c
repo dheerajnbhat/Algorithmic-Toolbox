fe964a0666de7a0cf121614a117874967bc680cd
Dheeraj Bhat <42843778+dheerajnbhat@users.noreply.github.com>
2018-09-26 20:21:39 +0530

    lcm.c

---
+++ LCM/lcm.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main()
+{
+	int a,b,lcm,i;
+	printf("Enter two numbers\n");
+	scanf("%d%d",&a,&b);	
+	i=gcd(a,b);	
+	lcm=(a*b)/i;	
+	printf("LCM of (%d,%d) is %d \n",a,b,lcm);
+	return 0;
+}
+
+int gcd(int a,int b)
+{
+	int q,r;
+	while(b>0)
+	{
+		q=a/b;
+		r=a-q*b;
+		a=b;
+		b=r;
+	}
+	return a;
+}		
