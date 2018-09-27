d5c9b3b67f04289acc86dce96a455d496500ab4d
Dheeraj Bhat <42843778+dheerajnbhat@users.noreply.github.com>
2018-09-27 20:04:28 +0530

    fibonacci.c

--- Fibonacci Series/fibonacci.c
+++ Fibonacci Series/fibonacci.c
@@ -2,20 +2,24 @@
 
 int main()
 {
-	int i,n;
-	int p,q;
+	int i,j,n,p,q;
 	printf("Enter n\n");
 	scanf("%d",&n);
+	printf("Enter Fp and Fq\n");
+	scanf("%d%d",&p,&q);	
 	printf("Fibonacci Series is \n");
 	for(i=0;i<=n;i++)
 		printf("%d\t",fib(i));
-	printf("\n");
-	printf("last digit of the fibonacci term is %d \n",(fib(n-1)+fib(n-2))%10);
-	printf("Enter Fp and Fq\n");
-	scanf("%d%d",&p,&q);
+	printf("\n");	
+	printf("The square of fibonacci series is \n");	
+	for(j=0;j<=n;j++)
+		printf("%d\t",fib(j)*fib(j));
+	printf("\n");	
+	printf("Fibonacci series from Fp to Fq is \n");	
 	for(i=p;i<=q;i++)
 		printf("%d\t",fib(i));
 	printf("\n");
+	printf("Last digit of the fibonacci term is %d \n",(fib(n-1)+fib(n-2))%10);	
 	return 0;
 }
 
@@ -25,3 +29,4 @@ int fib(int n)
 		return n;
 	return fib(n-1)+fib(n-2);
 }
+
